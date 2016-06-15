00f8df7296968e7d5dd3b2a33b576425226f6052
Matthew King <mattking73@gmail.com>
2016-06-15 16:04:59 -0500

    Added grunt/eslint

---
+++ Gruntfile.js
@@ -0,0 +1,27 @@
+module.exports = function (grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        eslint: {
+            options: {
+                configFile: '.eslintrc.json'
+            },
+            target: '.'
+        },
+        mochaTest: {
+            test: {
+                options: {
+                    reporter: 'spec',
+                    require: 'should'
+                },
+                src: [ 'test/**/*.js' ]
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-eslint');
+    grunt.loadNpmTasks('grunt-mocha-test');
+
+    grunt.registerTask('default', [ 'lint', 'mochaTest' ]);
+    grunt.registerTask('lint', 'eslint');
+    grunt.registerTask('test', 'mochaTest');
+};
