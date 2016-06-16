570f08bd8c5e19db35bd5a37fde169f5ad6fac33
Matthew King <mattking73@gmail.com>
2016-06-16 19:44:03 +0000

    Implemented short parts of rethink/memory/base drivers. Added a couple tests. Updated modes. Fix lint errors

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,9 +2,6 @@ module.exports = function (grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
         eslint: {
-            options: {
-                configFile: '.eslintrc.json'
-            },
             target: '.'
         },
         mochaTest: {
